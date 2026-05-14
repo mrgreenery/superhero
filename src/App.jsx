3071f2777df60deccf2d0e233a626658d3bd74a5
mrgreenery <186280196+mrgreenery@users.noreply.github.com>
2026-05-14 13:28:30 +0200

    fix HeroesDetailPage inmport

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { createHashRouter, RouterProvider } from 'react-router-dom'
 import Layout from './components/Layout'
 import HeroesPage from './pages/HeroesPage'
-import HeroDetailPage from './pages/HeroesDetailPage'
+import HeroesDetailPage from './pages/HeroesDetailPage'
 import AboutPage from './pages/AboutPage'
 
 const router = createHashRouter([
