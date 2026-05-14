bd30207b8e45650345701baabafe101024f801ef
mrgreenery <wilbergroen@gmail.com>
2026-05-14 14:20:13 +0200

    made count dynamic

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 export const SUPERHERO_TOKEN = 'b5aec36dee8ae084501e76aac7eb9711'
 export const API_BASE = 'https://akabab.github.io/superhero-api/api'
-export const TOTAL_HEROES = 731
 export const PAGE_SIZE = 20
